555324cb75e8b4cf6a7977b556f5958c601f2d95
Antoine BRUN <abrun@e3r7p8.clusters.42paris.fr>
2022-11-02 19:06:14 +0100

    UseGuards to handled the request

---
+++ backend/dist/auth/auth.controllers.d.ts
@@ -0,0 +1,9 @@
+import { Request } from "express";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+export declare class AuthController {
+    private authService;
+    constructor(authService: AuthService);
+    signup(dto: AuthDto): Promise<string>;
+    signin(dto: AuthDto): Promise<import(".prisma/client").User>;
+}
--- backend/src/auth/auth.controllers.ts
+++ backend/src/auth/auth.controllers.ts
@@ -1,9 +1,8 @@
-import { Body, Controller, Delete, Get, Post, UseGuards, Req, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Post, UseGuards, Req } from "@nestjs/common";
 import { Request } from "express";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
-import { AuthGuard } from "@nestjs/passport";
-import { GetUser } from "./decorator/get-user.decorator";
+import { AuthGuard } from "@nestjs/passport"; 
 
 @Controller('auth')
 export class AuthController {
@@ -17,31 +16,10 @@ export class AuthController {
   signin(@Body() dto: AuthDto) {
     return this.authService.signin(dto);
   }
-
 	// UseGuards determine if the request will be handled
 	@UseGuards(AuthGuard('42API'))
-	@Get('42api/login')
-	login42(@Req() request: Request) {
+	@Post('signupAPI')
+	signupAPI(@Req() request: Request) {
 		console.log({request});
-	}
-
-	@UseGuards(AuthGuard('42API'))
-	@Get('42api/redirect')
-	async handleRedirect (
-			@Query() query: {code: string, state: string},
-			@GetUser() user: {token: string, refreshToken: string}) {
-		// query contains code and state from oauth.strategy
-		console.log(query);
-		return (this.authService.logUser42(user.token));
-	}
-
-	@Get('google2FA/signup')
-	async signup2FA(@Body() body: {email: string}) {
-		return (this.authService.signup2FA(body.email));
-	}
-
-	@Post('google2FA/login')
-	verify2FA(@Body() body: {email: string, code: string}) {
-		return (this.authService.verify2FA(body));
 	}
 }
