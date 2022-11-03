104dd8b9e3d28ac5aca1d0ac311df9571ff12b24
Thomas CORDONNIER <tcordonn@e1r4p5.clusters.42paris.fr>
2022-11-03 15:09:00 +0100

    just create the user, and basic 'file upload'

--- backend/dist/auth/auth.controllers.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from "express";
-import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto";
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    signup(dto: AuthDto): Promise<string>;
-    signin(dto: AuthDto): Promise<import(".prisma/client").User>;
-}
--- backend/dist/auth/strategy/oauth.strategy.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from "../../prisma/prisma.service";
-declare const OAuthStrategy_base: new (...args: any[]) => import("passport-oauth2");
-export declare class OAuthStrategy extends OAuthStrategy_base {
-    private prismaService;
-    constructor(prismaService: PrismaService);
-    validate(content: any): void;
-}
-export {};
