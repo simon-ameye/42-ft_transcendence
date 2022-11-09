2716522a9960d1ad0f99dc44f9e8041a438b2f8a
Thomas CORDONNIER <tcordonn@e1r2p3.clusters.42paris.fr>
2022-11-09 13:17:19 +0100

    basic gateway for friendRequest notification

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module';
+import { GatewayModule } from './gateway/gateway.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
@@ -10,6 +11,7 @@ import { UserModule } from './user/user.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    GatewayModule,
     PrismaModule
   ],
 })
