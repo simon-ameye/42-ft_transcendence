2ab6a3db266853450e3f757d5fe936032af3fb7e
simon-ameye <ameye.simon@gmail.com>
2022-11-10 12:15:33 +0100

    removed game example module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,10 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ChatModule } from './chat/chat.module';
-import { GameModule } from './game/game.module';
 
 @Module({
   imports: [
 	ChatModule,
-	GameModule,
 	AuthModule,
     UserModule,
     ConfigModule.forRoot({ isGlobal: true, }),
--- backend/src/game/game.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GameGateway } from './game';
-
-@Module({
-	providers: [GameGateway],
-})
-export class GameModule {}
