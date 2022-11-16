953a32d3cd40f8de7ea4b3edb230267f3dfc1149
mbrizion <mbrizion@student.42.fr>
2022-11-16 05:14:32 +0100

    starting game. add some css for other pages, c chiant

--- frontend/src/components/Game/Canvas.tsx
+++ frontend/src/components/Game/Canvas.tsx
@@ -1,76 +1,56 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface props {
 	width:number;
 	height:number;
 }
 
-interface elementSize {
-	elemWidth:number;
-	elemHeight:number;
-}
-
-interface paddle extends elementSize {
-	posX:number;
-	posY:number;
-}
-
 const Canvas: React.FC<props> = ({width, height}) => {
 
-	const canvasWidth = width;
-	const canvasHeight = height;
-	const leftPaddle = initPaddle({posX: 10, posY: (canvasHeight / 2) - 50, elemHeight: 100, elemWidth: 10});
-	const rightPaddle = initPaddle({posX: canvasWidth - 20, posY: (canvasHeight / 2) - 50, elemHeight: 100, elemWidth: 10});
-	let move = 0;
+	const [paddlePosY, setPaddlePosY] = useState(0);
+	const leftPaddle = {posX: 10, posY: (height / 2) - 50, elemHeight: 100, elemWidth: 10};
+	const rightPaddle = {posX: width - 20, posY: (height / 2) - 50, elemHeight: 100, elemWidth: 10};
 
 
-	function initPaddle (leftPaddle: paddle){
-		return (leftPaddle);
-	}
-
-	const handleKeyPress = (event:React.KeyboardEvent<HTMLElement>) => {
-		if (event.key == "ArrowUp")
-		{
-			move++;
-		}
-		else if (event.key == "ArrowDown")
-		{
-
-		}
-	}
-
+	
 	const canvasRef = useRef<HTMLCanvasElement | null>(null);
 	let context:CanvasRenderingContext2D | null;
-
+	
 	useEffect(() => {
 		const canvas = canvasRef.current;
 		if (!canvas) 
-			throw new Error("Error")
+		throw new Error("Error")
 		context = canvas.getContext("2d");
 		if (!context)
-			throw new Error("Error")
-		draw(context, move);
-
+		throw new Error("Error")
+		draw(context);
+		
 	}, []);
+	const handleKeyPress = (event:React.KeyboardEvent<HTMLElement>) => {
+		if (event.key == "ArrowUp")
+		{
+			setPaddlePosY(paddlePosY + 100);
+			console.log(paddlePosY);
+			draw(context);
+			
+		}
+		//else if (event.key == "ArrowDown")
+		//{
+			
+		//}
+	}
 	
-	function draw(context:CanvasRenderingContext2D | null, move:number) {
+	function draw(context:CanvasRenderingContext2D | null) {
 		if (context)
 		{
 			context.fillStyle = "#333";
-			context.fillRect(0, 0, canvasWidth, canvasHeight);
+			context.fillRect(0, 0, width, height);
 			context.fillStyle = "#fff"
-			context.fillRect(leftPaddle.posX, leftPaddle.posY + move, leftPaddle.elemWidth, leftPaddle.elemHeight);
-			context.fillRect(rightPaddle.posX, rightPaddle.posY + move, rightPaddle.elemWidth, rightPaddle.elemHeight);
+			context.fillRect(leftPaddle.posX, leftPaddle.posY + paddlePosY, leftPaddle.elemWidth, leftPaddle.elemHeight);
+			context.fillRect(rightPaddle.posX, rightPaddle.posY, rightPaddle.elemWidth, rightPaddle.elemHeight);
 		}
 	}
 
-	function clearCanvas(context:CanvasRenderingContext2D | null){
-		if (context)
-		{
-			context.fillStyle = "#333";
-			context.fillRect(0, 0, canvasWidth, canvasHeight);
-		}
-	}
 	return (
 		<canvas 
 			tabIndex={0}
