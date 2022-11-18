fb689aa179f2a640ddd8de5bb0939da0cb8b88d0
Thomas CORDONNIER <tcordonn@e1r4p2.clusters.42paris.fr>
2022-11-18 13:57:37 +0100

    nothing special

---
+++ backend/prisma/migrations/20221118123916_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ALTER COLUMN "clientId" DROP NOT NULL;
--- backend/src/friend/friend.service.ts
+++ backend/src/friend/friend.service.ts
@@ -6,13 +6,13 @@ export class FriendService {
   constructor (private prisma: PrismaService) {}
 
   async sendFriendRequest( client, receiverId: string ) {
-    const relationship = await this.prisma.relationShip.create({
+    /*const relationship = await this.prisma.relationShip.create({
       data: {
         RequesterId: client.id,
         AddresseeId: receiverId,
         status: "Pending",
       }
-    })
+    })*/
   }
 }
 
