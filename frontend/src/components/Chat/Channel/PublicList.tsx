b18daef723219a487a300bdfec32dc86a7a87c34
simon-ameye <ameye.simon@gmail.com>
2022-12-01 11:59:16 +0100

    join public made prettier

--- frontend/src/components/Chat/Channel/PublicList.tsx
+++ frontend/src/components/Chat/Channel/PublicList.tsx
@@ -1,74 +1,64 @@
-import {ListItem} from '@mui/material'
-import ChannelInterface from '../ChannelInterface';
+import { ListItem } from '@mui/material'
 import ChannelsInterface from '../ChannelsInterface';
-import { IoIosAdd } from "react-icons/io";
 import axios from 'axios';
-import React, { useState } from 'react'
-import DeleteIcon from '@mui/icons-material/Delete';
-
-
-import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, InputAdornment, IconButton} from '@mui/material'
+import { useState } from 'react'
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, InputAdornment, IconButton } from '@mui/material'
 import VisibilityIcon from '@mui/icons-material/Visibility'
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 
-export default function PublicList () {
+export default function PublicList() {
 
   const [channelsInterfaces, setchannelsInterfaces] = useState<ChannelsInterface[]>([])
-
-
-
   const [open, setOpen] = useState(false);
   const [values, setValues] = useState({
-      channelId:0,
-      name:'',
+    channelId: 0,
+    name: '',
+    password: '',
+    showpass: false
+  })
+  const handleClickOpen = (channelId: number) => {
+    setValues({ ...values, channelId: channelId });
+    setOpen(true);
+  }
+  const handleVisibility = () => {
+    setValues({
+      ...values,
+      showpass: !values.showpass,
+    });
+  };
+  const handleClose = () => {
+    setValues({
+      channelId: 0,
+      name: '',
       password: '',
       showpass: false
     })
-  const handleClickOpen = (channelId : number) =>{
-      setValues({...values, channelId:channelId});
-      setOpen(true);
-    }
-    const handleVisibility = () => {
-		  setValues({
-			  ...values,
-			  showpass: !values.showpass,
-		  });
-	};
-  const handleClose = () => {
-		  setValues({
-      channelId:0,
-			name: '',
-			password: '',
-			showpass: false
-		})
-		setOpen(false)
-	}
+    setOpen(false)
+  }
   const handleJoinChannel = () => {
-    axios.post('http://localhost:3001/chat/joinChannel',{
+    axios.post('http://localhost:3001/chat/joinChannel', {
       channelId: values.channelId,
       password: values.password,
     }).then(res => console.log(res)).catch(err => console.log(err))
-}
+  }
   const handleSubmit = () => {
     handleClose()
     handleJoinChannel()
-}
-
-
-  function getPublicChannelTable() {
-    axios.get('http://localhost:3001/chat/getPublicChannelTable',{
-    }).then(
-      function (response) {
-        setchannelsInterfaces(response.data.channelsInterfaces);      }
-      )
   }
 
+  function getPublicChannelTable() {
+    axios.get('http://localhost:3001/chat/getPublicChannelTable', {
+    }).then(
+      function (response) {
+        setchannelsInterfaces(response.data.channelsInterfaces);
+      }
+    )
+  }
 
-  const channelList = channelsInterfaces.map((c , i) => (
-      <ListItem button key={i} onClick={event => handleClickOpen(c.id)} >{c.isProtected ? <div>P-</div> : <div>N-</div>} {c.name}
-      </ListItem>
-    ))
-
+  const channelList = channelsInterfaces.map((c, i) => (
+    <ListItem button key={i} onClick={event => handleClickOpen(c.id)} >{c.isProtected ? <div>P-</div> : <div>N-</div>} {c.name}
+    </ListItem>
+  ))
 
   return (
     <div className='PublicList'>
@@ -76,42 +66,42 @@ export default function PublicList () {
       <div>
         {channelList}
         <h2>
-        <Dialog open={open} onClose={handleClose}>
+          <Dialog open={open} onClose={handleClose}>
             <DialogTitle>Join Channel</DialogTitle>
-                <DialogContent>
-                <DialogContentText>
-                    Enter channel password
-				</DialogContentText>
-                <TextField
-                    type={values.showpass ? "text" : "password"}
-                    fullWidth
-                    label="Password"
-                    placeholder="Password"
-                    variant="outlined"
-                    style={{
-                        padding: 5
-                    }}
-                    InputProps={{
-						        endAdornment: (
-							<InputAdornment position="end">
-								<IconButton onClick={handleVisibility}
-								  aria-label="toggle password"
-								  edge="end">
-									{values.showpass ? <VisibilityOffIcon /> : <VisibilityIcon />}
-								</IconButton>
-							</InputAdornment>
-						)
-					}}
-					onChange={(e:any) => {
-						setValues({...values, password: e.target.value})
-					}}
-            />
+            <DialogContent>
+              <DialogContentText>
+                Enter channel password
+              </DialogContentText>
+              <TextField
+                type={values.showpass ? "text" : "password"}
+                fullWidth
+                label="Password"
+                placeholder="Password"
+                variant="outlined"
+                style={{
+                  padding: 5
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={handleVisibility}
+                        aria-label="toggle password"
+                        edge="end">
+                        {values.showpass ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+                onChange={(e: any) => {
+                  setValues({ ...values, password: e.target.value })
+                }}
+              />
             </DialogContent>
             <DialogActions>
-				<button onClick={handleClose}>Cancel</button>
-				<button onClick={handleSubmit}>Join</button>
-			</DialogActions>
-        </Dialog>
+              <button onClick={handleClose}>Cancel</button>
+              <button onClick={handleSubmit}>Join</button>
+            </DialogActions>
+          </Dialog>
         </h2>
       </div>
     </div>
