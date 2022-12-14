1480265f38278a25db1497a3a2fd814dead4a2d8
simon-ameye <ameye.simon@gmail.com>
2022-12-14 14:27:27 +0100

    profile

--- backend/src/profile/profile.controllers.ts
+++ backend/src/profile/profile.controllers.ts
@@ -5,6 +5,9 @@ import { GetUser } from "src/auth/decorators/get-user.decorator";
 import { UserDto } from "src/auth/dto";
 import { ProfileService } from "./profile.service";
 import { ProfileInterface } from "./interfaces/profile.interface";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { UploadedFile } from "@nestjs/common";
+import { UseInterceptors } from "@nestjs/common";
 
 @Controller('profile')
 export class ProfileController {
@@ -14,5 +17,19 @@ export class ProfileController {
   @Get('getProfile')
   async getProfile(@GetUser() user: UserDto) {
     return (this.profileService.getProfile(Number(user.id)));
+  }
+
+  //@UseGuards(AuthGuard('jwt'))
+  //@Post('uploadPicture')
+  //uploadPicture(@Body() body: { UserId: number, },
+  //  @GetUser() user: UserDto) {
+  //  return (this.profileService.uploadPicture(user.id, ));
+  //}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('uploadPicture')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 }
