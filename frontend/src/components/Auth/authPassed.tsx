4e9ee0879a7a5b0ac5ddbda2d7dbca3afb185d9f
Thomas CORDONNIER <tcordonn@e1r5p7.clusters.42paris.fr>
2023-01-12 12:58:52 +0100

    API key now only in .env

--- frontend/src/components/Auth/authPassed.tsx
+++ frontend/src/components/Auth/authPassed.tsx
@@ -20,8 +20,8 @@ export default function AuthPassed () {
 		if (code && state && !cookie.login) {
 			axios.post('https://api.intra.42.fr/oauth/token', {
 				grant_type: "authorization_code",
-				client_id: "u-s4t2ud-648c51ea9e1ba58cce46cff68acc6882c3fc4382864770ac7e8f610111a703ec",
-				client_secret: "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",
+				client_id: process.env.REACT_APP_CLIENT_ID,
+				client_secret: process.env.REACT_APP_CLIENT_SECRET,
 				code: code,
 				state: state,
 				redirect_uri: "http://localhost:3000/auth"
